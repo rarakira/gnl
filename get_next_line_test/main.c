de282b2d23c39e2b6296669105f35ed79f07a837
Kira <rarakira@gmail.com>
2021-06-02 10:54:25 +0300

    Update main to read stdin

--- get_next_line_test/main.c
+++ get_next_line_test/main.c
@@ -8,32 +8,59 @@ int		main(void)
 	int		i = 0;
 	int		res;
 
-	int		fd = open("text1", O_RDONLY);
-
-	while ((res = get_next_line(fd, &line)) != 0)
-	{
-		if (res == -1)
-        {
-			printf("%d (%d) %s\n", i++, res, line);
-            free(line);
-            return (0);
-        }
-		printf("%d (%d) %s\n", i++, res, line);
-		free(line);
-	}
-	printf("%d (%d) %s\n", i++, res, line);
-	free(line);
-	
-	//int		fds[2];
-	//fds[0] = open("text1", O_RDONLY);
-	//fds[1] = open("text2", O_RDONLY);
-	//while (res = get_next_line(fds[i % 2], &line))
+	//int		fd = open("text1", O_RDONLY);
+//
+	//while ((res = get_next_line(fd, &line)) != 0)
 	//{
-	//	printf("%d (%d) %s\n", i++, res, line);
+	//	if (res == -1)
+    //    {
+	//		printf("%d | %d (%d) %s\n", fd, i++, res, line);
+    //        free(line);
+    //        return (0);
+    //    }
+	//	printf("%d | %d (%d) %s\n", fd, i++, res, line);
 	//	free(line);
 	//}
-	//printf("%d (%d) %s\n", i++, res, line);
+	//printf("%d | %d (%d) %s\n", fd, i++, res, line);
 	//free(line);
+	
+	int		fds[2];
+	int		a;
+	fds[0] = open("text1", O_RDONLY);
+	fds[1] = open("text2", O_RDONLY);
+	while ((res = get_next_line(fds[i % 2], &line)) != 0)
+	{
+		if ((i % 2) == 1)
+			a = 1;
+		else
+			a = 0;
+		if (res == -1)
+		{
+			printf("%d | %d (%d) %s\n", fds[a], i++, res, line);
+			free(line);
+			return (0);
+		}
+		printf("%d | %d (%d) %s\n", fds[a], i++, res, line);
+		free(line);
+	}
+	printf("%d | %d (%d) %s\n", fds[a], i++, res, line);
+	free(line);
+
+	//while ((res = get_next_line(0, &line)) != 0)
+	//{
+	//	if (res == -1)
+    //    {
+	//		printf("%d | %d (%d) %s\n", 0, i++, res, line);
+    //        free(line);
+    //        return (0);
+    //    }
+	//	printf("%d | %d (%d) %s\n", 0, i++, res, line);
+	//	free(line);
+	//}
+	//printf("%d | %d (%d) %s\n", 0, i++, res, line);
+	//free(line);
+
+	//getchar();
 
 	return (0);
 }
